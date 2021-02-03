a2e1d43ba4ad39c2a932eabe82a6a075b8e7aaee
p-cap <slashedeye@gmail.com>
2021-02-02 22:44:33 -0800

    changed my project route

---
+++ src/components/Info/ProjectTitles.js
@@ -0,0 +1,21 @@
+import Swift from '../assets/Swift_logo.svg';
+import Python from '../assets/Python.svg';
+import Material from '../assets/Material_ui.svg';
+import SpringBoot from '../assets/Springboot.svg';
+
+
+export const ProjectTitles =[
+    "Data Structure and Algo" ,
+    "Material-UI",
+    "Spring Boot",
+    "Swift/UI"
+]
+
+export const Images = [
+    Python, 
+    Material,
+    SpringBoot,
+    Swift
+]
+
+export default ProjectTitles
--- src/components/ui/routes/Projects.js
+++ src/components/ui/routes/Projects.js
@@ -5,11 +5,8 @@ import { Paper, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { useMediaQuery } from "@material-ui/core";
 
-//images
-import Swift from '../../assets/Swift_logo.svg';
-import Python from '../../assets/Python.svg';
-import Javascript from '../../assets/Javascript.svg';
-import Java from '../../assets/Java.svg';
+// Project titles
+import ProjectTitles, { Images } from '../../Info/ProjectTitles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,13 +15,13 @@ const useStyles = makeStyles((theme) => ({
   titleText: {
    ...theme.titleText,
     [theme.breakpoints.down("md")]: {
-      fontSize: "4.0rem"
+      fontSize: "4.5rem"
     },
     [theme.breakpoints.down("sm")]: {
-      fontSize: "2.5rem"
+      fontSize: "3.5rem"
     },
     [theme.breakpoints.down("xs")]: {
-      fontSize: "2.0rem"
+      fontSize: "3.0rem"
     },
   },
 
@@ -48,20 +45,16 @@ const useStyles = makeStyles((theme) => ({
 
   comingSoonText: {
     ...theme.otherFont,
-    fontSize: "4rem"
+    fontSize: "3rem"
   },
   images: {
     [theme.breakpoints.down("md")]: {
-      height: "75px",
-      width: "75px"
-    },
-    [theme.breakpoints.down("sm")]: {
-      height: "70px",
-      width: "70px"
+      height: "150px",
+      width: "200px"
     },
     [theme.breakpoints.down("xs")]: {
-      height: "65px",
-      width: "65px"
+      height: "150px",
+      width: "200px"
     },
   }
 }));
@@ -81,100 +74,57 @@ export default function Projects() {
         justify="center"
         alignItems="center"
         spacing={3}>
-        <Grid item>
-          <Typography 
-            variant="h2" 
-            className={classes.titleText}
-            >
-              Projects</Typography>
-        </Grid>
-        <Grid item>
-          <Grid 
-            container
-            direction={isExtraSmallScreen ? "column": "row"}
-            justify="center"
-            alignItems="center"
-            spacing={3}>
             <Grid item>
-                  <Grid 
-                    container="column"
-                    justify="center"
-                    alignItems="center"
-                    spacing={2}
-                    >                
-                    <Paper className={classes.paper}>
-                      <Box p={1}>
-                        <a href="https://github.com/p-cap/resolution2021" style={{ textDecoration: 'none'}}>
-                          <Grid item>
-                                  <Grid 
-                                    container
-                                    justify="center"
-                                    alignItems="center"
-                                  >
-                                      <img alt="Swift" src={Swift} className={classes.images}/>
-                                      <img alt="Python" src={Python} className={classes.images} />
-                                      <img alt="Javascript" src={Javascript} className={classes.images}/>
-                                      <img alt="Java" src={Java} className={classes.images}/>
+              <Typography 
+                variant="h2" 
+                className={classes.titleText}
+                >
+                  Projects</Typography>
+            </Grid>
+            <Grid item>
+              <Grid 
+                container
+                direction={isExtraSmallScreen ? "column": "row"}
+                justify="center"
+                alignItems="center"
+                spacing={4}>
+                   { ProjectTitles.map((title, index) => (
+                        <Grid item key={index}>
+                            <Grid
+                              container
+                              justify="center"
+                              alignItems="center"
+                            >
+                              <Paper className={classes.paper} >
+                                <Grid item>
+                                  <Typography 
+                                    color="primary" 
+                                    align="center"
+                                    className={classes.comingSoonText}
+                                    > {title}
+                                  </Typography>
                                   </Grid>
-                          </Grid>
-                          <Grid item>
-                                <Typography color="primary" align="center">
-                                      <Typography className={classes.synopsisText}>Resolution 2021 </Typography><br/>
-                                      Swift / SwiftUi mobile applications <br/>
-                                      Data Structures and Algorithms with Python, Java and Javascript <br/>
-                                      SYNOPSIS: This repo consists of small snippets of code for practice.
-                                      Quantitative practice leads to qualitative results <br/>
-                                </Typography>
-                           </Grid>
-                          </a>
-                          </Box>
-                        </Paper>
-                   </Grid>
-            </Grid>
-            <Grid item>
-            <Grid item>
-                <Grid 
-                    container
-                    justifyContent="center"
-                    alignItems="center"
-                    >
-                    <Grid item>
-                        <Paper className={classes.paper} >
-                          <Typography 
-                            color="primary" 
-                            align="center"
-                            className={classes.comingSoonText}
-                            >
-                            Coming Soon ;=)
-                            </Typography>
-                        </Paper>
+                                  <Grid item>
+                                    <Grid 
+                                      container
+                                      justify="center"
+                                      alignItems="center"
+                                      >
+                                      <Box p={3}>
+                                      <img alt="Image" src={Images[index]} className={classes.images}/>
+                                      </Box>
+                                    </Grid>
+                                  </Grid>
+                            </Paper>
+                        </Grid>
                       </Grid> 
+                    ))}
                   </Grid>
               </Grid>
             </Grid>
-            <Grid item>
-                <Grid 
-                    container
-                    justifyContent="center"
-                    alignItems="center"
-                    >
-                    <Grid item>
-                        <Paper className={classes.paper} >
-                          <Typography 
-                            color="primary" 
-                            align="center"
-                            className={classes.comingSoonText}
-                            >
-                            Coming Soon ;=)
-                            </Typography>
-                        </Paper>
-                      </Grid> 
-                  </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-          </Grid>
-    </div>
+  
+
+    </div>  
     )
 }
 

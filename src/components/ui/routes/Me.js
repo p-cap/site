7566253bb5792b4b2ef72a2b653082e5105e1a32
p-cap <slashedeye@gmail.com>
2021-02-07 20:48:35 -0800

    fixed breakpoints for me page

--- src/components/ui/routes/Me.js
+++ src/components/ui/routes/Me.js
@@ -34,15 +34,19 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("xl")]: {
       top: "4rem",
     },
-    [theme.breakpoints.down("xs")]: {
-      top: "20px",
-    },
+    // [theme.breakpoints.down("xs")]: {
+    //   top: "20px",
+    // },
     left: "0px",
     width: "100%",
     fontWeight: "500",
     fontSize: "2rem",
+    // [theme.breakpoints.down("sm")]: {
+    //   fontSize: "1.5rem",
+    // },
     [theme.breakpoints.down("xs")]: {
-      fontSize: "1.5rem",
+      fontSize: "1.0rem",
+      top: "100px"
     },
   },
   
